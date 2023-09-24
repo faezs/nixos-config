725a9fd384f94572978f44e4970a839d1ee17088
Faez Shakil <faez.shakil@gmail.com>
2023-09-24 13:42:58 +0500

    hack for sizing

--- conf/doom.d/config.el
+++ conf/doom.d/config.el
@@ -74,7 +74,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(set-face-attribute 'default (selected-frame) :height 35)
+(set-face-attribute 'default (selected-frame) :height 100)
 
 ;; accept completion from copilot and fallback to company
 (use-package! copilot
